7b9856594ee53bfc8fbbbfdb06c0b2b902894b90
Jay Soffian <jay@soffian.org>
2026-03-20 18:03:30 +0000

    Remove unused CannotConnectError class

--- custom_components/ha_lumagen/const.py
+++ custom_components/ha_lumagen/const.py
@@ -1,14 +1,8 @@
 """Constants for the Lumagen integration."""
 
-from homeassistant.exceptions import HomeAssistantError
-
 DOMAIN = "ha_lumagen"
 DEFAULT_PORT = 4999
 
 ERROR_CANNOT_CONNECT = "cannot_connect"
 
 CONF_ASPECT_RATIOS = "aspect_ratios"
-
-
-class CannotConnectError(HomeAssistantError):
-    """Error to indicate we cannot connect to the device."""
